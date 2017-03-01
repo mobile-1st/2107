8a0c271b8c7d41743fe916ddc1c4a65e8f4b585f
Wei Sun <wsun@CNwsun.local>
2017-03-01 20:13:02 +0800

    for start page; Wayde

--- pages/start/index.js
+++ pages/start/index.js
@@ -12,7 +12,7 @@ Page({
     onLoad() {},
     onShow() {},
     bindload(e) {
-    	setTimeout(App.WxService.getStorageSync('token') ? this.goIndex : this.goLogin, 300000)
+    	setTimeout(App.WxService.getStorageSync('token') ? this.goIndex : this.goLogin, 3000)
     },
     goIndex() {
         App.WxService.switchTab({
