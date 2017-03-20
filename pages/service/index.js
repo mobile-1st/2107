2b8afe331b50251c47fc29e74ebbcd8c031b0f76
Wei Sun <wsun@CNwsun.local>
2017-03-20 21:34:08 +0800

    change names; Wayde

--- pages/service/index.js
+++ pages/service/index.js
@@ -5,6 +5,7 @@ Page({
         service: {},
         navData: {
             house: {
+                title: '家政服务',
                 name: '吴阿姨',
                 phone: '13545361669',
                 type: '保洁',
@@ -12,6 +13,7 @@ Page({
                 description: '吴阿姨工作认真仔细，服务至今，已经收到不少好评。热烈欢迎您的点评，和我们一起鼓励客户至上的专业服务！'
             },
             fixing: {
+                title: '维修服务',
                 name: '廖师傅',
                 phone: '13618643166',
                 type: '修锁',
