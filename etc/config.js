afa521a6da277597ced90bdb8c4add53c09778ae
Wei Sun <wsun@CNwsun.local>
2017-04-19 22:21:01 +0800

    add topic; Wayde

--- etc/config.js
+++ etc/config.js
@@ -1,5 +1,6 @@
 export default {
 	basePath: 'https://api.sunzhongmou.com',
 	fileBasePath: 'https://api.sunzhongmou.com/',
-	appId: 'wx87ec248ea93e7398'
+	appId: 'wx87ec248ea93e7398',
+	ihakula_request: 'ihakula_northern_hemisphere',
 }
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,6 @@
+import Config from '../../etc/config'
+var util = require('../../utils/util.js');
+
 const App = getApp()
 
 Page({
@@ -10,18 +13,23 @@ Page({
         hotHouse: [
             {id: 0, type: 'house', name: '2107. 当代国际花园', description: '南北朝向 - 1室1厅1卫 - 未来域 - 精装温馨现房 - 地铁，公交配套出行方便', avatar: 'https://source.sunzhongmou.com/2107-source/2107_home.png'}
         ],
+        postsList: [],
+        hidden: false,
+        page: 1,
+        tab: 'all',
         assets: {}
     },
     onLoad() {
-        this.assets = App.HttpResource('/user/get-user-asset/:id', {id: '@id'})
-        this.getAssets()
+        // this.assets = App.HttpResource('/user/get-user-asset/:id', {id: '@id'})
+        // this.getAssets()
+        this.posts = App.HttpResource('/bbs/event/get/:id', {id: '@id'})
+        this.fetchData()
     },
     onShow() {
     },
     navigateTo(e) {
         const type = e.currentTarget.dataset.type
         if(type === 'house'){
-            console.log('?????')
         } else {
             const id = e.currentTarget.dataset.id
             if( id == 2 && this.data.assets.contracts.length == 0){
@@ -43,6 +51,32 @@ Page({
                 })
             })
     },
+    fetchData: function () {
+    var self = this;
+    self.setData({
+      hidden: false
+    });
+    if (this.page === 1) {
+      self.setData({
+        postsList: []
+      });
+    }
+    this.posts.queryAsync({
+        "ihakula_request":Config.ihakula_request,
+        "params_string":'{"tab":"zy"}',
+        "url":"https://bbs.sunzhongmou.com/api/v1/topics"
+    })
+            .then(res => {
+                console.log(res)
+                self.setData({
+                    postsList: self.data.postsList.concat(res.data.map(function (item) {
+                        console.log(item.last_reply_at)
+                        item.last_reply_at = util.getDateDiff(new Date(item.last_reply_at));
+                        return item;
+                        }))
+        });
+            })
+  },
     onTapTag(e) {
     },
     showWarning() {
