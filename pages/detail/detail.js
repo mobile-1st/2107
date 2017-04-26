87c6bfa70feea6da42955153de7466c9e3fa3872
Wei Sun <wsun@CNwsun.local>
2017-04-26 22:21:34 +0800

    add comment; Wayde

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -6,38 +6,43 @@ const App = getApp()
 
 
 Page({
-  data: {
-    title: '话题详情',
-    detail: {},
-    hidden: false,
-    wxParseData:[]
-  },
-  onLoad: function (options) {
-    this.detailReq = App.HttpResource('/bbs/event/get/:id', {id: '@id'})
-    this.fetchData(options.id);
-  },
-  fetchData: function (id) {
-    var self = this;
-    self.setData({
-      hidden: false
-    });
+    data: {
+        title: '话题详情',
+        detail: {},
+        hidden: false,
+        wxParseData: []
+    },
+    onLoad: function (options) {
+        this.detailReq = App.HttpResource('/bbs/event/get/:id', {id: '@id'})
+        this.fetchData(options.id);
+    },
+    fetchData: function (id) {
+        var self = this;
+        self.setData({
+            hidden: false
+        });
 
-this.detailReq.queryAsync({
-        "ihakula_request":Config.ihakula_request,
-        "params_string":'{"mdrender":false}',
-        "url":"https://bbs.sunzhongmou.com/api/v1/topic/" + id
-    })
+        this.detailReq.queryAsync({
+            "ihakula_request": Config.ihakula_request,
+            "params_string": '{"mdrender":false}',
+            "url": "https://bbs.sunzhongmou.com/api/v1/topic/" + id
+        })
             .then(res => {
                 console.log(res);
-        res.data.create_at = util.getDateDiff(new Date(res.data.create_at));
-        res.data.replies = res.data.replies.map(function (item) {
-            item.create_at = util.getDateDiff(new Date(item.create_at));
-            return item;
-          })
-        self.setData({
-          detail: res.data,
-          wxParseData: WxParse('md',res.data.content.replace(/\/public\//g, "https://bbs.sunzhongmou.com/public/"))
-        });
-        });
-  }
+                res.data.create_at = util.getDateDiff(new Date(res.data.create_at));
+                res.data.replies = res.data.replies.map(function (item) {
+                    item.create_at = util.getDateDiff(new Date(item.create_at));
+                    return item;
+                })
+                self.setData({
+                    detail: res.data,
+                    wxParseData: WxParse('md', res.data.content.replace(/\/public\//g, "https://bbs.sunzhongmou.com/public/"))
+                });
+            });
+    },
+    bindComment: function(e) {
+        App.WxService.navigateTo('/pages/comment/index', {
+            id: "the-id"
+        })
+    }
 })
