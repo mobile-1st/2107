c44fef4c941752cd61d0039eec3dfbf80ca9cf55
Wei Sun <wsun@CNwsun.local>
2017-04-30 09:32:12 +0800

    for srvice; Wayde

--- pages/service/index.js
+++ pages/service/index.js
@@ -2,46 +2,36 @@ const App = getApp()
 
 Page({
     data: {
-        service: {},
-        navData: {
-            house: {
-                title: '家政服务',
-                name: '吴阿姨',
-                phone: '13545361669',
-                type: '保洁',
-                price: '30-50元/小时',
-                description: '吴阿姨工作认真仔细，服务至今，已经收到不少好评。热烈欢迎您的点评，和我们一起鼓励客户至上的专业服务！'
-            },
-            fixing: {
-                title: '维修服务',
-                name: '廖师傅',
-                phone: '13618643166',
-                type: '修锁',
-                price: '视锁的情况定',
-                description: '廖师傅已经由公安认证。热烈欢迎您的点评，和我们一起鼓励客户至上的专业服务！'
-            },
-        }
+        items: [
+            { text: '小区物业报修电话', path: '02786698926' },
+            { text: '物业客服值班电话', path: '02788186415' },
+            { text: '区房管投诉受理电话', path: '02781818240' },
+            { text: '市房管投诉监督电话', path: '02785482119' },
+        ]
     },
-    bindtap: function(e) {
-        App.WxService.makePhoneCall({
-            phoneNumber: this.data.service.phone
-		})
-    },
-    onLoad(options) {
-        let key = ''
-        const id = options.id
-        switch(id){
-            case '0':
-                key = 'house'
-                break;
-            case '1':
-                key = 'fixing'
-                break;
-        }
-        this.setData({
-            'service': this.data.navData[key]
-        })
+    onLoad() {
     },
     onShow() {
+    },
+    onPullDownRefresh() {
+    },
+    onReachBottom() {
+    },
+    navigateTo(e) {
+        const path = e.currentTarget.dataset.path
+
+        App.WxService.makePhoneCall({
+            phoneNumber: path
+        })
+    },
+    onShareAppMessage: function () {
+        return {
+            title: '中冶创业苑 - 畅居',
+            path: '/pages/service/index',
+            success: function(res) {
+            },
+            fail: function(res) {
+            }
+        }
     }
 })
