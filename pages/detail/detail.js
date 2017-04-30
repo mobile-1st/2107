c91fbb584b87faa06ebdb3e16ec98ccf7fecd2ca
Wei Sun <wsun@CNwsun.local>
2017-04-30 11:54:57 +0800

    add read; Wayde

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -15,6 +15,7 @@ Page({
     },
     onLoad: function (options) {
         this.detailReq = App.HttpResource('/bbs/event/get/:id', {id: '@id'})
+        this.readReq = App.HttpResource('/bbs/event/post/:id', {id: '@id'})
         this.setData({
             topicId: options.id
         })
@@ -30,20 +31,29 @@ Page({
             "ihakula_request": Config.ihakula_request,
             "params_string": '{"mdrender":false}',
             "url": "https://bbs.sunzhongmou.com/api/v1/topic/" + this.data.topicId
-        })
-            .then(res => {
-                console.log(res);
-                res.data.create_at = util.getDateDiff(new Date(res.data.create_at));
-                res.data.replies = res.data.replies.map(function (item) {
-                    item.create_at = util.getDateDiff(new Date(item.create_at));
-                    return item;
-                })
-                self.setData({
-                    detail: res.data,
-                    wxParseData: WxParse('md', res.data.content.replace(/\/public\//g, "https://bbs.sunzhongmou.com/public/").replace(/http:/g, "https:"))
-                })
-                console.log(self.data.wxParseData)
-            });
+        }).then(res => {
+            console.log(res);
+            res.data.create_at = util.getDateDiff(new Date(res.data.create_at));
+            res.data.replies = res.data.replies.map(function (item) {
+                item.create_at = util.getDateDiff(new Date(item.create_at));
+                return item;
+            })
+            self.setData({
+                detail: res.data,
+                wxParseData: WxParse('md', res.data.content.replace(/\/public\//g, "https://bbs.sunzhongmou.com/public/").replace(/http:/g, "https:"))
+            })
+            console.log(self.data.wxParseData)
+        });
+
+        this.readReq.saveAsync({
+            "ihakula_request": Config.ihakula_request,
+            "params_string": JSON.stringify({
+                "accesstoken": App.WxService.getStorageSync('accessToken'),
+            }),
+            "url": "https://bbs.sunzhongmou.com/api/v1/topic/read/" + this.data.topicId
+        }).then(res => {
+            console.log(res);
+        });
     },
     bindComment: function(e) {
         App.WxService.navigateTo('/pages/comment/index', {
